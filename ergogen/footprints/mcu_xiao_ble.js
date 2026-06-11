e2506df293d3a9c740090b274791f39626c4c504
Jerry Wang <jerrylwang123@gmail.com>
2026-06-11 19:06:37 -0400

    feat(ergogen): enable_nfc for mcu_xiao_ble footprint

--- ergogen/footprints/mcu_xiao_ble.js
+++ ergogen/footprints/mcu_xiao_ble.js
@@ -3,6 +3,7 @@ module.exports = {
         designator: 'MCU',
         side: 'F',
         reversible: false,
+        enable_nfc: true,
 
         P0: { type: 'net', value: 'P0' },
         P1: { type: 'net', value: 'P1' },
@@ -57,11 +58,38 @@ module.exports = {
         `;
 
         // through hole pads for pads on back
+        const nfc_pads = `
+            (pad "21" thru_hole circle (at 3.802408 8.801408 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.NFC1})
+            (pad "22" thru_hole circle (at 5.707408 8.801408 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.NFC2})
+        `;
         const flippable_pads = `
             (pad "19" thru_hole circle (at -4.445 -0.317 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.BAT_POS})
             (pad "20" thru_hole circle (at -4.445 -2.222 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.BAT_NEG})
-            (pad "21" thru_hole circle (at 3.802408 8.801408 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.NFC1})
-            (pad "22" thru_hole circle (at 5.707408 8.801408 ${p.r}) (size 1.397 1.397) (drill 1.016) (layers "*.Cu" "*.Mask") (remove_unused_layers no)  ${p.NFC2})
+            ${p.enable_nfc ? nfc_pads : ""}
+        `;
+
+        // parts on both sides
+        const top = `
+            (footprint "mcu_xiao_ble"
+                (layer "${p.side}.Cu")
+                ${p.at /* parametric position */}
+                (property "Reference" "${p.ref}"
+                  (at 0 -15 ${p.r})
+                  (layer "${p.side}.SilkS")
+                  ${p.ref_hide}
+                  (effects (font (size 1 1) (thickness 0.15)))
+                )
+                (attr exclude_from_pos_files exclude_from_bom)
+
+                ${'' /* controller outline */}
+                (fp_rect (start -8.89 10.5) (end 8.89 -10.5) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
+                ${'' /* 9.0018 by 1.5079 usb connector overhang */}
+                (fp_rect (start -4.5009 -10.5) (end 4.5009 -12.0079) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
+                ${'' /* weird controller pads that don't do anything */}
+                (fp_rect (start -5.285811 -6.785813) (end -3.507811 -4.118813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
+                (fp_rect (start -3.507811 -8.182813) (end -5.285811 -10.849813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
+                (fp_rect (start 3.350197 -10.849813) (end 5.128197 -8.182813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
+                (fp_rect (start 3.350197 -6.785813) (end 5.128197 -4.118813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
         `;
 
         // kicad drc error: board edge clearance violation near arc edge cuts should be false alarm
@@ -77,6 +105,16 @@ module.exports = {
             (fp_arc (start 1.524 -8.1402) (mid 0.920405 -8.222308) (end 0.838133 -8.825881) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
         `;
 
+        const nfc_cuts = `
+            (fp_line (start 3.548408 6.769408) (end 3.548408 8.369608) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_line (start 3.929408 6.388408) (end 5.580408 6.388408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_line (start 4.234208 9.055408) (end 5.275608 9.055408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_line (start 5.961408 6.769408) (end 5.961408 8.369608) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_arc (start 3.548408 6.769392) (mid 3.660009 6.499987) (end 3.929424 6.388408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_arc (start 3.548408 8.369608) (mid 4.152091 8.451796) (end 4.234301 9.055476) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_arc (start 5.275608 9.055408) (mid 5.35788 8.451784) (end 5.961527 8.369675) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            (fp_arc (start 5.580408 6.388408) (mid 5.849808 6.500004) (end 5.961392 6.769408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+        `;
         const flippable_cuts = `
             ${'' /* bat */}
             (fp_line (start -4.699 -1.7902) (end -4.699 -0.7488) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
@@ -87,43 +125,10 @@ module.exports = {
             (fp_arc (start -4.0132 -2.476) (mid -4.095312 -1.872402) (end -4.698889 -1.790137) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
             (fp_arc (start -2.413 -2.476) (mid -2.143592 -2.364407) (end -2.032 -2.094998) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
             (fp_arc (start -2.032 -0.444) (mid -2.143593 -0.174592) (end -2.413002 -0.063) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-
-            ${'' /* nfc */}
-            (fp_line (start 3.548408 6.769408) (end 3.548408 8.369608) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_line (start 3.929408 6.388408) (end 5.580408 6.388408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_line (start 4.234208 9.055408) (end 5.275608 9.055408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_line (start 5.961408 6.769408) (end 5.961408 8.369608) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_arc (start 3.548408 6.769392) (mid 3.660009 6.499987) (end 3.929424 6.388408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_arc (start 3.548408 8.369608) (mid 4.152091 8.451796) (end 4.234301 9.055476) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_arc (start 5.275608 9.055408) (mid 5.35788 8.451784) (end 5.961527 8.369675) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
-            (fp_arc (start 5.580408 6.388408) (mid 5.849808 6.500004) (end 5.961392 6.769408) (stroke (width 0.12) (type solid)) (layer "Edge.Cuts") )
+            ${p.enable_nfc ? nfc_cuts : ""}
         `;
 
 
-
-        // parts on both sides
-        const top = `
-            (footprint "mcu_xiao_ble"
-                (layer "${p.side}.Cu")
-                ${p.at /* parametric position */}
-                (property "Reference" "${p.ref}"
-                  (at 0 -15 ${p.r})
-                  (layer "${p.side}.SilkS")
-                  ${p.ref_hide}
-                  (effects (font (size 1 1) (thickness 0.15)))
-                )
-                (attr smd exclude_from_pos_files exclude_from_bom)
-
-                ${'' /* controller outline */}
-                (fp_rect (start -8.89 10.5) (end 8.89 -10.5) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-                ${'' /* 9.0018 by 1.5079 usb connector overhang */}
-                (fp_rect (start -4.5009 -10.5) (end 4.5009 -12.0079) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-                ${'' /* weird controller pads that don't do anything */}
-                (fp_rect (start -5.285811 -6.785813) (end -3.507811 -4.118813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-                (fp_rect (start -3.507811 -8.182813) (end -5.285811 -10.849813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-                (fp_rect (start 3.350197 -10.849813) (end 5.128197 -8.182813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-                (fp_rect (start 3.350197 -6.785813) (end 5.128197 -4.118813) (stroke (width 0.12) (type solid)) (fill no) (layer "Dwgs.User") )
-        `;
 
         // parts only on front
         const front = `
@@ -182,3 +187,4 @@ function flipped_y_axis(s, flip) {
 
     return s.replaceAll(re, replacer);
 }
+
